e11e8a294349ef899b6f9f448ef79f9430be9025
AndreMatioski02 <matioskiandre4@gmail.com>
2022-06-17 11:27:01 -0300

    Initial commit

---
+++ src/ts/app.ts
@@ -0,0 +1,36 @@
+const pokemonCard = document.querySelector('.pokemon-cards')
+
+const fetchPokemon = () => {
+
+   const promises = [];
+   for (let i = 1; i <= 150; i++) {
+      const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+      promises.push(fetch(url).then(res => res.json()));
+   }
+
+   Promise.all(promises).then(results => {
+      const pokemon = results.map(data => ({
+         name: data.name,
+         id: data.id,
+         image: data.sprites["front_default"],
+         type: data.types.map((type: { type: { name: any; }; }) => type.type.name).join(", "),
+      }));
+      displayPokemon(pokemon);
+   });
+};
+
+const displayPokemon = (pokemon: { name: any; id: any; image: any; type: any; }[]) => {
+   const pokemonHTMLString = pokemon
+   .map(
+      pokeman => `
+         <div class="card">
+            <img class="pokemon-image" src="${pokeman.image}" alt="Pokemon">
+            <hr class="hr-style">
+            <p class="pokemon-description">${pokeman.id}. ${pokeman.name}</p>
+            <p class="pokemon-description">Type: ${pokeman.type}</p>
+         </div>
+      `).join("");
+   pokemonCard.innerHTML = pokemonHTMLString;
+};
+
+fetchPokemon()
